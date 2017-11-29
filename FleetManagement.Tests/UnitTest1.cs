a7a241afd4ec14a7d011e7a0baad50d50a7b5136
chris <chris@DESKTOP-BFS84U9>
2017-11-29 16:37:05 +0100

    Projektdateien hinzufügen.

---
+++ FleetManagement.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FleetManagement.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
